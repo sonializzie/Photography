f6a78294796e87e498d93a32d045ba999f6c0312
Sonia Ndonga <soniandonga@yahoo.com>
2021-08-13 14:43:31 +0100

    Added files to the repository

---
+++ src/script.js
@@ -0,0 +1,7 @@
+function homePage() {
+  window.location = "file:///Users/sonia/Free%20Time/Photography/homepage.html";
+}
+
+function mainPage() {
+  window.location = "file:///Users/sonia/Free%20Time/Photography/index.html";
+}
